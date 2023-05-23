e2f901bdd5b8452500895045c69898bca70c9995
Paweł Wielga <pawell1029@gmail.com>
2023-05-23 20:03:02 +0200

    przeniesienie zdjęć z dolnoslaskie do img

---
+++ niemanudy/rename.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Directory path where the files are located
+directory="C:\Users\Wielgowie\source\repos\website\niemanudy\dolnoslaskie"
+
+# New file name prefix
+new_prefix="dolnoslaskie_"
+
+# Navigate to the directory
+cd "$directory" || exit
+
+# Iterate over all files in the directory
+for filename in *.jpg; do
+    # Generate the new file name
+    new_filename="${new_prefix}${filename}"
+
+    # Rename the file
+    mv "$filename" "$new_filename"
+    echo "Renamed $filename to $new_filename"
+done
