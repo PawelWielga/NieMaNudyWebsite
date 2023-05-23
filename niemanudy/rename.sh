3cccc5d66217bb2896d6ddc55d6dfd20e51dec89
Paweł Wielga <pawell1029@gmail.com>
2023-05-23 20:26:02 +0200

    przeniesienie zdjęć z podkarpackie do img

--- niemanudy/rename.sh
+++ niemanudy/rename.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Directory path where the files are located
-directory="C:\Users\Wielgowie\source\repos\website\niemanudy\mazowieckie"
+directory="C:\Users\Wielgowie\source\repos\website\niemanudy\podkarpackie"
 
 # New file name prefix
-new_prefix="mazowieckie_"
+new_prefix="podkarpackie_"
 
 # Navigate to the directory
 cd "$directory" || exit
